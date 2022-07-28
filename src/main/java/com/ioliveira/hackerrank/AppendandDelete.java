b5213d3eb0648e35aaa5ebebb591a4c9e8dc598a
Igor Oliveira <igorfoliveira@gmail.com>
2022-07-28 04:27:36 -0300

    AppendandDelete

---
+++ src/main/java/com/ioliveira/hackerrank/AppendandDelete.java
@@ -0,0 +1,44 @@
+package com.ioliveira.hackerrank;
+
+
+public class AppendandDelete {
+
+    //https://www.hackerrank.com/challenges/append-and-delete/
+
+    public static String appendAndDelete(String s, String t, int k) {
+        StringBuilder str = new StringBuilder(s);
+
+        if (!str.toString().equals(t)) {
+            for (int i = 0; i < k; i++) {
+                if (t.contains(str) && str.toString().equals(t.substring(0, str.length()))) {
+                    if (str.length() <= 1 && k - i > t.length()) {
+                        return "Yes";
+                    }
+                    if (str.length() == t.length()) {
+                        str.append(t.charAt(str.length() - 1));
+                    } else {
+                        str.append(t.charAt(str.length()));
+                    }
+                } else {
+                    str.deleteCharAt(str.length() - 1);
+                }
+            }
+        }
+
+        if (str.toString().equals(t) || s.equals(t)) {
+            return "Yes";
+        }
+
+        return "No";
+    }
+
+    public static void main(String[] args) {
+        //hackerhappy
+        //hackerrank
+
+        //System.out.println(appendAndDelete("hackerhappy", "hackerrank", 9));
+        //System.out.println(appendAndDelete("aaaaaaaaaa", "aaaaa", 7));
+        //System.out.println(appendAndDelete("abcdef", "fedcba", 15));
+        System.out.println(appendAndDelete("aaa", "a", 5));
+    }
+}
